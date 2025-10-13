5c4adab8864ffe8c5841a350b1e0d7a1eeb8c2a1
Joaquín Emilio Huayllasco López <joaquin20040107@gmail.com>
2025-10-12 21:37:24 -0500

    Agregando dependencias

---
+++ src/main/java/pe/MIKHUY/MIKHUYApplication.java
@@ -0,0 +1,13 @@
+package pe.MIKHUY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MIKHUYApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MIKHUYApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/MIKHUY/MIKHUYApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.MIKHUY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MIKHUYApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
