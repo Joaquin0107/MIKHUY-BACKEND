0232e1bfce93038173b067e623d76330c8f540de
Joaquin <joaquin20040107@gmail.com>
2026-05-10 23:37:02 -0500

    Corrigiendo perfil

--- src/main/java/pe/MIKHUY/Security/SecurityConfig.java
+++ src/main/java/pe/MIKHUY/Security/SecurityConfig.java
@@ -71,6 +71,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/estudiantes/perfil").authenticated() // ← AGREGAR ESTA LÍNEA
+
                         // 🔹 Endpoints con roles
                         .requestMatchers("/api/estudiantes/**").hasAnyAuthority("student","admin", "teacher")
                         .requestMatchers("/api/juegos/**").hasAnyAuthority("student","teacher","admin")
