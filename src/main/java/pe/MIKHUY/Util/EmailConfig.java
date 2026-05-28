b6d6bade65f161f26fa014cdd61d84ebd2d3c637
Joaquin <joaquin20040107@gmail.com>
2026-05-27 21:42:40 -0500

    Configurando Envío de Correo

--- src/main/java/pe/MIKHUY/Util/EmailConfig.java
+++ src/main/java/pe/MIKHUY/Util/EmailConfig.java
@@ -12,17 +12,26 @@ import java.util.Properties;
 public class EmailConfig {
 
     public JavaMailSender createCustomMailSender(String email, String password, String host, Integer port) {
+        log.info("🔧 Creando MailSender personalizado para: {}", email);
+
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         String smtpHost = host != null ? host : "smtp.gmail.com";
         Integer smtpPort = port != null ? port : 587;
+
         mailSender.setHost(smtpHost);
         mailSender.setPort(smtpPort);
         mailSender.setUsername(email);
         mailSender.setPassword(password);
+
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.trust", smtpHost);
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        log.info("✅ MailSender personalizado creado para: {}", email);
         return mailSender;
     }
 }
